d934c4806af8985806456256cf4805c5e28d8b8a
Paul Fariello <paul@fariello.eu>
2022-10-08 23:55:26 +0200

    Allow access to storage from async and store OMEMO contact devices

--- src/storage/schema.rs
+++ src/storage/schema.rs
@@ -1,9 +1,24 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    omemo_device (id) {
+    omemo_contact_device (id) {
         id -> Integer,
         account -> Text,
+        contact -> Text,
         device_id -> Integer,
     }
 }
+
+diesel::table! {
+    omemo_own_device (id) {
+        id -> Integer,
+        account -> Text,
+        device_id -> Integer,
+        current -> Bool,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    omemo_contact_device,
+    omemo_own_device,
+);
