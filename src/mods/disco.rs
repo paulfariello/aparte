371784186e66dd729e44926505cd9da9d4351f6a
Paul Fariello <paul@fariello.eu>
2024-02-16 17:52:56 +0100

    Add real signal storage

--- src/mods/disco.rs
+++ src/mods/disco.rs
@@ -49,7 +49,7 @@ impl DiscoMod {
     }
 
     pub fn disco(&mut self, jid: Jid, node: Option<String>) -> Element {
-        let id = Uuid::new_v4().to_hyphenated().to_string();
+        let id = Uuid::new_v4().hyphenated().to_string();
         let query = disco::DiscoInfoQuery { node };
         let iq = Iq::from_get(id, query).with_to(Jid::from_str(&jid.domain()).unwrap());
         iq.into()
--- src/storage/schema.rs
+++ src/storage/schema.rs
@@ -10,11 +10,12 @@ diesel::table! {
 }
 
 diesel::table! {
-    omemo_contact_prekey (contact_prekey_pk) {
-        contact_prekey_pk -> Integer,
-        contact_device_fk -> Integer,
-        id -> BigInt,
-        data -> Binary,
+    omemo_identity (identity_pk) {
+        identity_pk -> Integer,
+        account -> Text,
+        user_id -> Text,
+        device_id -> BigInt,
+        identity -> Binary,
     }
 }
 
@@ -27,10 +28,51 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(omemo_contact_prekey -> omemo_contact_device (contact_device_fk));
+diesel::table! {
+    omemo_pre_key (pre_key_pk) {
+        pre_key_pk -> Integer,
+        account -> Text,
+        pre_key_id -> BigInt,
+        pre_key -> Binary,
+    }
+}
+
+diesel::table! {
+    omemo_sender_key (sender_key_pk) {
+        sender_key_pk -> Integer,
+        account -> Text,
+        sender_id -> Text,
+        device_id -> BigInt,
+        distribution_id -> Binary,
+        sender_key -> Binary,
+    }
+}
+
+diesel::table! {
+    omemo_session (session_pk) {
+        session_pk -> Integer,
+        account -> Text,
+        user_id -> Text,
+        device_id -> BigInt,
+        session -> Binary,
+    }
+}
+
+diesel::table! {
+    omemo_signed_pre_key (signed_pre_key_pk) {
+        signed_pre_key_pk -> Integer,
+        account -> Text,
+        signed_pre_key_id -> BigInt,
+        signed_pre_key -> Binary,
+    }
+}
 
 diesel::allow_tables_to_appear_in_same_query!(
     omemo_contact_device,
-    omemo_contact_prekey,
+    omemo_identity,
     omemo_own_device,
+    omemo_pre_key,
+    omemo_sender_key,
+    omemo_session,
+    omemo_signed_pre_key,
 );
